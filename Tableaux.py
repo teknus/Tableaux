453f56002ef278c3ee206d14ff0dda4494414296
teknus <mateusteknus@gmail.com>
2017-06-15 13:26:45 -0300

    V1.0

--- Tableaux.py
+++ Tableaux.py
@@ -3,9 +3,32 @@ pilha_ramos = list()
 nos = 0
 max_nos = 0
 num_i = []
-l_ramos = []
 # -1 False
 # 1 True
+
+#Essa classe torna possivel ordenar as formulas de entrada
+class FormulaDeEntrada:
+    def __init__(self, value,form):
+        self.value = 1 if value >= 1 else -1
+        self.form = form
+    def __it__(self,other):
+        return len(self.form) < len(other.form)
+    def __le__(self,other):
+        return len(self.form) <= len(other.form)
+    def __eq__(self,other):
+        return len(self.form) == len(other.form)
+    def __ne__(self,other):
+        return len(self.form) != len(other.form)
+    def __ge__(self,other):
+        return len(self.form) >= len(other.form)
+    def __gt__(self,other):
+        return len(self.form) > len(other.form)
+    def __str__(self):
+        return str(self.value) + " " + self.form
+    def __repr__(self):
+        return (str(self.value) + " " + self.form)
+
+
 def is_alpha(value, op):
     if op in ["|" ,"!" ,">",""] and value == -1:
         return True
@@ -38,6 +61,7 @@ def ramo_fechado(main_branch):
 
 
 def split_form(input_tuple):
+    #Retorna a posicao do operador,(>,|,&,!)se hover um 
     if len(input_tuple) == 2:
         input_string = input_tuple[1]
     else:
@@ -64,26 +88,29 @@ def split_form(input_tuple):
     return list(reversed(stack_form))[0]
 
 def format_formula(form):
-        op_pos = split_form(form)
-        if len(form) == 2:
-            if op_pos == 0:
-                if len(form[1]) == 4:
-                    return(form[0],form[1][op_pos],form[1][2:len(form[1]) -1 ],"")
-                f = form[1][2:len(form[1]) - 1]
-                return(form[0],form[1][0],f,"")
-            if is_uni(form):
-                return form
-            return (form[0],form[1][op_pos],form[1][1:op_pos],form[1][op_pos+1:len(form[1]) -1])
-
-        #       (operador,formula esquerda, formula direita)
+    #Retorna a fomula em um formato que torna as  operacoes sobre elas mais facil
+    # Este formato eh (operador,formula esquerda, formula direita)
+    op_pos = split_form(form)
+    if len(form) == 2:
+        if op_pos == 0:
+            if len(form[1]) == 4:
+                return(form[0],form[1][op_pos],form[1][2:len(form[1]) -1 ],"")
+            f = form[1][2:len(form[1]) - 1]
+            return(form[0],form[1][0],f,"")
+        if is_uni(form):
+            return form
+        return (form[0],form[1][op_pos],form[1][1:op_pos],form[1][op_pos+1:len(form[1]) -1])
 
 def is_uni(form):
+    #Faz a checagem se a formula eh unitaria
     if len(form) > 2:
         return False
     else:
         return len(form[1]) <= 1
 
 def add_to_main(f1,f2,main_branch):
+    #Tratamento para formulas alfas serem adicionadas diretamente no 
+    #Ramo principal
     if len(f1) >= 1 and f1 != "":
         main_branch.append(f1)
     if len(f2) >= 1 and f2 != "":
@@ -92,7 +119,6 @@ def add_to_main(f1,f2,main_branch):
 
 def solve(main_branch,nos_fechados,betas,num_ramos=1,i=0):
     #alfas
-    print(main_branch)
     n = len(main_branch)
     while i < n:
         if i > nos:
@@ -117,7 +143,6 @@ def solve(main_branch,nos_fechados,betas,num_ramos=1,i=0):
                 while j > 0:
                     if is_uni(main_branch[j]):
                         if temp[0] * -1 == main_branch[j][0] and temp[1] == main_branch[j][1]:
-                            #print("alfa",i,j,main_branch)
                             nos_fechados[i] = 1
                             nos_fechados[j] = 1
                     j -= 1    
@@ -125,7 +150,6 @@ def solve(main_branch,nos_fechados,betas,num_ramos=1,i=0):
         n = len(main_branch)
     #Betas
     while len(betas) > 0:
-        print("Beta")
         i = betas[-1]
         nos_fechados[i] = 1
         betas.remove(i)
@@ -148,11 +172,7 @@ def solve(main_branch,nos_fechados,betas,num_ramos=1,i=0):
                     test = True
                 else:
                     pilha_ramos.append((main_branch[:]+[f1],[f2],nos_fechados))
-                l_ramos.append(i)
-                print("Teste de ramo ",ramo_fechado(pilha_ramos[-1][0]))
                 f,ramo,n,num_ramos = solve(pilha_ramos[-1][0],nos_fechados,betas,num_ramos,i)
-                print("Retorno do solve ",f)
-                #print("f1",f,ramo,ramo_fechado(ramo),f2)
                 if not f :
                     if test:
                         main_branch = ramo+[g1]+[g2]
@@ -170,12 +190,7 @@ def solve(main_branch,nos_fechados,betas,num_ramos=1,i=0):
                     test = True
                 else:
                     pilha_ramos.append((main_branch[:]+[f2],[f1],nos_fechados))
-                l_ramos.append(i)
-                print("Teste de ramo ",ramo_fechado(pilha_ramos[-1][0]))
                 f,ramo,n,num_ramos = solve(pilha_ramos[-1][0],nos_fechados,betas,num_ramos,i)
-                print("Retorno do solve ",f)
-                #print("f2",f,ramo,ramo_fechado(ramo),f1)
-
                 if not f :
                     if test:
                         main_branch = ramo+[g1]+[g2]
@@ -198,10 +213,13 @@ def solve(main_branch,nos_fechados,betas,num_ramos=1,i=0):
     return ramo_fechado(pilha_ramos[-1][0]),main_branch,nos_fechados,num_ramos
 
 def expand_formula(value,op,left,right,alfa):
+    #Trata de dar a valoracao correta para as novas formulas
+    # com expancao baseada no operador e valor da formula original
     def atribuir_valores(val, form):
         if type(form) == type(list()) or type(form) == type(tuple()):
             return [val] + [x for x in form]
         return (val,form)
+    # esse caso trata quando a formula e uma negacao
     if right == "":
         return "",atribuir_valores(value * -1, left)
 
@@ -222,19 +240,20 @@ def expand_formula(value,op,left,right,alfa):
 
 def numero_nos(main_branch):
     t = ""
-    for s in [x[1] for x in main_branch]:
+    for s in [x.form for x in main_branch]:
         t += s
     return sum([1 for y in t if y != "(" and y != ")"])
 
 
 def run(main_branch):
+    sorted(main_branch)
     nos = numero_nos(main_branch)
+    main_branch = [(f.value,f.form) for f in main_branch]
     betas = list()
-    nos_fechados = [-1 for x in range(0,numero_nos(main_branch))]
+    nos_fechados = [-1 for x in range(0,nos)]
     v,m,n,num_ramos = solve(main_branch,nos_fechados,betas)
     if v:
         print("Valida",len(pilha_ramos),[x[0] for x in pilha_ramos],nos,num_ramos)
-        #print(max(num_i),len(num_i),l_ramos,len(l_ramos))
     elif len(pilha_ramos) > 1:
         print("Refutada: ", nos,num_ramos)
         print("Valoracao: ",len(pilha_ramos), list(set([ x for x in pilha_ramos[-1][0]+pilha_ramos[-1][1] if is_uni(x)])))
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from Tableaux import *
 # | ou
 # & e
 #Fomulas Notavel de teste
-main_branch = [(1,"(a|b)"),(1, '(a>(c|d))'),(1, "(b>(c|d))"),(1,"(c>(e|f))"),(1,"(d>(e|f))"),(-1,"(e|f)")]
-#main_branch = [(1,"(c>b)"),(-1,"b"),(1,"c")]
+
+main_branch = [FormulaDeEntrada(1, '(a>(c|d))'),FormulaDeEntrada(1, "(b>(c|d))"),FormulaDeEntrada(1,"(c>(e|f))"),FormulaDeEntrada(1,"(d>(e|f))"),FormulaDeEntrada(-1,"(e|f)"),FormulaDeEntrada(1,"(a|b)")]
+main_branch = [FormulaDeEntrada(1,"(c>d)"),FormulaDeEntrada(1,"c"),FormulaDeEntrada(-1,"d")]
 run(main_branch)
 #lista de nos fechados
 #Pilha de ramos
